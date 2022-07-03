96c00b3ba4895a14f4385e7c11b9db8123166441
Hillary Mongare <hillarymongare70@gmail.com>
2022-07-03 14:15:23 +0300

    first commit

---
+++ src/components/forms/LocationForm.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+import useInput from "../../hooks/use-input";
+import classes from "./LocationForm.module.css";
+
+const LocationForm = (props) => {
+  const [formHasError, setFormHasError] = useState(false);
+
+  const {
+    value: enteredLatitude,
+    isValid: enteredLatitudeIsValid,
+    hasError: latitudeInputHasError,
+    valueChangeHandler: latitudeInputChangeHandler,
+    inputBlurHandler: latitudeInputBlurHandler,
+    reset: resetLatitudeInput,
+  } = useInput(
+    (value) => value.trim() !== "" && isFinite(value) && Math.abs(value) <= 90
+  );
+
+  const {
+    value: enteredLongitude,
+    isValid: enteredLongitudeIsValid,
+    hasError: longitudeInputHasError,
+    valueChangeHandler: longitudeInputChangeHandler,
+    inputBlurHandler: longitudeInputBlurHandler,
+    reset: resetLongitudeInput,
+  } = useInput(
+    (value) => value.trim() !== "" && isFinite(value) && Math.abs(value) <= 180
+  );
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredLatitudeIsValid || !enteredLongitudeIsValid) {
+      setFormHasError(true);
+      return;
+    }
+
+    const locationData = {
+      latitude: enteredLatitude,
+      longitude: enteredLongitude,
+    };
+
+    // props.onAddLocationData(locationData);
+    console.log(locationData);
+
+    resetLatitudeInput();
+    resetLongitudeInput();
+  };
+
+  const latitudeInputClasses = latitudeInputHasError
+    ? classes["form-control-invalid"]
+    : classes["form-control"];
+
+  const longitudeInputClasses = longitudeInputHasError
+    ? classes["form-control-invalid"]
+    : classes["form-control"];
+
+  return (
+    <form autoComplete="off" onSubmit={submitHandler}>
+      {formHasError && (
+        <p className={classes["error-text"]}>
+          Please enter the required data below.
+        </p>
+      )}
+      <div className={latitudeInputClasses}>
+        <label htmlFor="latitude">Latitude:</label>
+        <input
+          type="text"
+          id="latitude"
+          value={enteredLatitude}
+          onBlur={latitudeInputBlurHandler}
+          onChange={latitudeInputChangeHandler}
+        />
+        {latitudeInputHasError && (
+          <p className={classes["error-text"]}>Enter a valid latitude!</p>
+        )}
+      </div>
+      <div className={longitudeInputClasses}>
+        <label htmlFor="longitude">Longitude:</label>
+        <input
+          type="text"
+          id="longitude"
+          value={enteredLongitude}
+          onBlur={longitudeInputBlurHandler}
+          onChange={longitudeInputChangeHandler}
+        />
+        {longitudeInputHasError && (
+          <p className={classes["error-text"]}>Enter a valid longitude!</p>
+        )}
+      </div>
+      <div className={classes["form-actions"]}>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default LocationForm;
