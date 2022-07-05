35bb589a6d25604c4d7df51082cd96ae24978ad8
Hillary Mongare <hillarymongare70@gmail.com>
2022-07-05 21:05:48 +0300

    v1 final build

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     setIsLoading(true);
 
     const response = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${locationData.latitude}&longitude=${locationData.longitude}&hourly=temperature_2m&hourly=relativehumidity_2m&hourly=windspeed_120m&hourly=cloudcover_mid`
+      `${process.env.REACT_APP_API_URI}&latitude=${locationData.latitude}&longitude=${locationData.longitude}`
     );
 
     const data = await response.json();
@@ -37,97 +37,114 @@ const App = () => {
       <section className={classes.section1}>
         <Card>
           <h1 className={classes.title}>CyWeather Weather Forecast</h1>
-          <p className={classes.intro}>
+          <p>
             Hi, <br /> Welcome to the CyWeather Weather Forecast. <br /> Want to
             find today's weather forecast? <br />
-            Look no further just enter the required details below.
+            Look no further just enter the required details below and click
+            Submit.
           </p>
           <LocationForm onAddLocationData={submitLocationDataHandler} />
         </Card>
       </section>
       <section className={classes.section2}>
         {loadedForecast ? (
-          <Fragment>
-            <h5>latitude: {loadedForecast.latitude}</h5>
-            <h5>Longitude: {loadedForecast.longitude}</h5>
+          <Card>
+            <h1 className={classes.title}>Awesome, here are the results!</h1>
+            <h4>Latitude: {loadedForecast.latitude}</h4>
+            <h4>Longitude: {loadedForecast.longitude}</h4>
             <div className={classes["table-data"]}>
               <table>
-                <tr>
-                  <th>Time (ISO 8601)</th>
-                </tr>
-
-                {loadedForecast.hourly.time.slice(6, 19).map((val, key) => {
-                  return (
-                    <tr key={key}>
-                      <td>{val}</td>
-                    </tr>
-                  );
-                })}
-              </table>
-              <table>
-                <tr>
-                  <th>Temperature (°C)</th>
-                </tr>
-
-                {loadedForecast.hourly.temperature_2m
-                  .slice(6, 19)
-                  .map((val, key) => {
+                <thead>
+                  <tr>
+                    <th>Time (ISO 8601)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loadedForecast.hourly.time.slice(6, 19).map((val, key) => {
                     return (
                       <tr key={key}>
                         <td>{val}</td>
                       </tr>
                     );
                   })}
+                </tbody>
               </table>
               <table>
-                <tr>
-                  <th>Wind speed (km/h)</th>
-                </tr>
-
-                {loadedForecast.hourly.windspeed_120m
-                  .slice(6, 19)
-                  .map((val, key) => {
-                    return (
-                      <tr key={key}>
-                        <td>{val}</td>
-                      </tr>
-                    );
-                  })}
+                <thead>
+                  <tr>
+                    <th>Temperature (°C)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loadedForecast.hourly.temperature_2m
+                    .slice(6, 19)
+                    .map((val, key) => {
+                      return (
+                        <tr key={key}>
+                          <td>{val}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
               </table>
               <table>
-                <tr>
-                  <th>Humidity (%)</th>
-                </tr>
-
-                {loadedForecast.hourly.relativehumidity_2m
-                  .slice(6, 19)
-                  .map((val, key) => {
-                    return (
-                      <tr key={key}>
-                        <td>{val}</td>
-                      </tr>
-                    );
-                  })}
+                <thead>
+                  <tr>
+                    <th>Wind speed (km/h)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loadedForecast.hourly.windspeed_120m
+                    .slice(6, 19)
+                    .map((val, key) => {
+                      return (
+                        <tr key={key}>
+                          <td>{val}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
               </table>
               <table>
-                <tr>
-                  <th>Cloud cover (%)</th>
-                </tr>
-
-                {loadedForecast.hourly.cloudcover_mid
-                  .slice(6, 19)
-                  .map((val, key) => {
-                    return (
-                      <tr key={key}>
-                        <td>{val}</td>
-                      </tr>
-                    );
-                  })}
+                <thead>
+                  <tr>
+                    <th>Humidity (%)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loadedForecast.hourly.relativehumidity_2m
+                    .slice(6, 19)
+                    .map((val, key) => {
+                      return (
+                        <tr key={key}>
+                          <td>{val}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Cloud cover (%)</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {loadedForecast.hourly.cloudcover_mid
+                    .slice(6, 19)
+                    .map((val, key) => {
+                      return (
+                        <tr key={key}>
+                          <td>{val}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
               </table>
             </div>
-          </Fragment>
+          </Card>
         ) : (
-          <p>No result! </p>
+          <h1 className={classes.warning}>No result! </h1>
         )}
       </section>
     </Fragment>
